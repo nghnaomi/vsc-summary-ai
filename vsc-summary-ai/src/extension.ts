02d8b399d5dc61cd1e37239f7cd80c054be9b02f
Naomi Ng <nghnaomi04@gmail.com>
2025-03-22 17:26:42 +0800

    ???

--- vsc-summary-ai/src/extension.ts
+++ vsc-summary-ai/src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vsc-summary-ai.helloWorld', summarise);
+	const disposable = vscode.commands.registerCommand('vsc-summary-ai.helloWorld', () => summarise);
 
 	context.subscriptions.push(disposable);
 }
